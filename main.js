5026c4a0fc657528ef12ffead84922e50eaecc3e
Janiel Maia <janielmaia2023@gmail.com>
2024-11-19 12:53:03 -0300

    :tada: primerito commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const menuBtn = document.getElementById("nav_menu_btn");
+const navLinks = document.getElementById("nav_links");
+const menuBtnIcon = document.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
